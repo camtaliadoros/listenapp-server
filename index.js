3f5208ff65e7150cb1769906b1c167ab65c41199
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2023-10-13 15:08:39 +0100

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
-const port = 3000;
+const port = 80;
 
 require('dotenv').config();
 
